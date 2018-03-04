0c1a870d41f2bf2bb579d81e57bb41aac25e37f6
Mathieu VACANCE <mathieu.vacance@epitech.eu>
2018-03-04 18:00:01 +0100

    buttons left toolbars associated func

--- src/main_loop.c
+++ src/main_loop.c
@@ -163,50 +163,6 @@ sfIntRect	set_rectangle(sfIntRect rect, int top, int left, int width, int height
 	return (rect);
 }
 
-/*game_object_t **create_game_object()
-{
-	game_object_t **rooms = malloc(sizeof(game_object_t)* 6);
-	sfIntRect rect_larger;
-	sfIntRect rect_smaller;
-	sfIntRect rect_rotate_right;
-	sfIntRect rect_rotate_left;
-	sfIntRect rect_up;
-	sfIntRect rect_down;
-
-	rect_larger = set_rectangle(rect_larger, 0,0,100,100);
-	rooms[0] = create_object("./picture/larger.png", (sfVector2f){-250, -300}, rect_larger);
-	rect_smaller = set_rectangle(rect_smaller, 0,0,100,100);
-	rooms[1] = create_object("./picture/smaller.png", (sfVector2f){-250, -300}, rect_smaller);
-	rect_rotate_right = set_rectangle(rect_rotate_right, 0,0,100,100);
-	rooms[2] = create_object("./picture/return.png", (sfVector2f){-250, -300}, rect_rotate_right);
-	rect_rotate_left = set_rectangle(rect_rotate_left, 0,0,100,100);
-	rooms[3] = create_object("./picture/return.png", (sfVector2f){-250, -300}, rect_rotate_left);
-	rect_up = set_rectangle(rect_up, 0,0,100,100);
-	rooms[4] = create_object("./picture/return.png", (sfVector2f){-250, -300}, rect_up);
-	rect_down = set_rectangle(rect_down, 0,0,100,100);
-	rooms[5] = create_object("./picture/return.png", (sfVector2f){-250, -300}, rect_down);
-	return (rooms);
-}
-
-button_t **create_buttons(game_object_t **rooms)
-{
-	button_t **elem = malloc(sizeof(button_t) * 6);
-	button_t button_larger;
-	button_t button_smaller;
-	button_t button_rotate_right;
-	button_t button_rotate_left;
-	button_t button_up;
-	button_t button_down;
-
-	elem[0] = buttonInitialise(&button_larger, (sfVector2f){10, 200}, (sfVector2f){100, 100}, larger, rooms[0]);
-	elem[1] = buttonInitialise(&button_smaller, (sfVector2f){10, 310}, (sfVector2f){100, 100}, smaller, rooms[1]);
-	elem[2] = buttonInitialise(&button_rotate_right, (sfVector2f){10, 410}, (sfVector2f){100, 100}, rotate_l, rooms[2]);
-	elem[3] = buttonInitialise(&button_rotate_left, (sfVector2f){10, 510}, (sfVector2f){100, 100}, rotate_r, rooms[3]);
-	elem[4] = buttonInitialise(&button_up, (sfVector2f){10, 610}, (sfVector2f){100, 100}, rotate_u, rooms[4]);
-	elem[5] = buttonInitialise(&button_down, (sfVector2f){10, 710}, (sfVector2f){100, 100}, rotate_d, rooms[5]);
-	return (elem);
-}*/
-
 int	main_loop(void)
 {
 	surface_t *win = init_window();
@@ -247,8 +203,6 @@ int	main_loop(void)
 	elem[4] = buttonInitialise(&button_up, (sfVector2f){10, 610}, (sfVector2f){100, 100}, rotate_u, rooms[4]);
 	elem[5] = buttonInitialise(&button_down, (sfVector2f){10, 710}, (sfVector2f){100, 100}, rotate_d, rooms[5]);
 
-	//rooms = create_game_object();
-	//elem = create_buttons(rooms);
 	if (win == NULL)
 		return (84);
 	sfRenderWindow_setFramerateLimit(win->window, 60);
