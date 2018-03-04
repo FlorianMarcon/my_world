539d97fb36d1824e127ab0c81f7b617b3315bb56
marcon <florian.marcon@epitech.eu>
2018-03-04 21:05:02 +0100

    create function display water

--- Makefile
+++ Makefile
@@ -21,6 +21,7 @@ SRC	=	$(WAY)/main.c		\
 		$(WAY)/draw/draw_bottom_map.c	\
 		$(WAY)/draw/draw_background.c	\
 		$(WAY)/draw/display.c	\
+		$(WAY)/draw/draw_water.c	\
 		$(WAY)/draw/draw_left_toolbar.c	\
 		$(WAY)/key_event/mouse.c		\
 		$(WAY)/save_map/save_map.c		\
--- src/draw/display.c
+++ src/draw/display.c
@@ -6,13 +6,15 @@
 */
 
 #include "header_world.h"
+int	draw_water(sfRenderWindow *window, map_t *map);
 
 int	display(surface_t *win, map_t *map, states_t *matter)
 {
-	evolve_two_d_map(map);
 	draw_background(map, win);
+	draw_water(win->window, map);
 	draw_bottom_map(win->window, map, matter);
 	draw_2d_map(win->window, map, matter);
 	//draw_square_selection();
+	evolve_two_d_map(map);
 	return (0);
 }
